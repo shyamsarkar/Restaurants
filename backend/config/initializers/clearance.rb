f48d489c51cc895e48af09c7534e4cc1662d2745
Shyam Sarkar <69477996+shyamsarkar@users.noreply.github.com>
2024-06-04 01:05:27 +0530

    Enabled Cors (#59)

--- backend/config/initializers/clearance.rb
+++ backend/config/initializers/clearance.rb
@@ -1,5 +1,5 @@
 Clearance.configure do |config|
-  config.mailer_sender = 'reply@example.com'
+  config.mailer_sender = 'user@shyamraipur.restaurants'
   config.rotate_csrf_on_sign_in = false
   config.routes = false
   # Add more parameters here
