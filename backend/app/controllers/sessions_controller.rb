5623622bed868b84d84f58ef6b47d6d541811c21
Shyam Sarkar <amisam15sarkar05@gmail.com>
2025-04-18 21:37:59 +0530

    removed duplicate code

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class SessionsController < Clearance::SessionsController
-  skip_before_action :verify_authenticity_token
 
   def create
     email = params[:session][:email].downcase
